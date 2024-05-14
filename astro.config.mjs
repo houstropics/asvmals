20a356f884eedf30655a75d2080f17b0863316e8
houstropics <165116600+houstropics@users.noreply.github.com>
2024-05-14 16:55:03 +0000

    /

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://houstropics.github.io',
-  base: 'asvmals',
+  base: '/asvmals',
 });
 
 
